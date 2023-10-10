77a0eb8f62ba213add39416db9188e7f0f15c4fa
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-10-10 08:04:28 +0530

    Route for get all users

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -36,3 +36,33 @@ export const onBoardUser = async (req,res,next) => {
         next(error);
     }
 }
+
+
+export const getAllUsers =async (req,res,next) => {
+    try {
+        const prisma = getPrismaInstance();
+        const users = await prisma.user.findMany({
+            orderBy : {name: "asc"},
+            select : {
+                id:true,
+                email:true,
+                name:true,
+                profilePicture:true,
+                about:true,
+            }
+        });
+        const usersGroupByInitialLetter = {};
+
+        users.forEach((user)=>{
+            const initialLetter = user.name.charAt(0).toUpperCase();
+            if(!usersGroupByInitialLetter[initialLetter]){
+                usersGroupByInitialLetter[initialLetter] = [];
+            }
+            usersGroupByInitialLetter[initialLetter].push(user)
+        });
+        return res.status(200).send({users: usersGroupByInitialLetter})
+        
+    } catch (error) {
+        next(error);
+    }
+}
